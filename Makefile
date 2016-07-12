955bbb21dc96022654a3dddc0a61a3b47664c096
Alexandr Kolosov <rikorsev@mail.ru>
2016-07-12 17:09:46 +0300

    lktm_semaphores has been debuged

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-obj-m := scull.o hello.o sync.o threads.o lktm_semaphores.o
+obj-m := lktm_scull.o lktm_hello.o lktm_sync.o lktm_threads.o lktm_semaphores.o
 MY_CFLAGS += -g -DDEBUG
 ccflags += ${MY_CFLAGS}
 cc += ${MY_CFLAGS}
--- lktm_semaphores.c
+++ lktm_semaphores.c
@@ -12,14 +12,18 @@ static struct semaphore sem;
 static struct task_struct* th_a = NULL;
 static struct task_struct* th_b = NULL;
 
+static void __exit sem_clenup(void);
+
 static int th_b_entery(void* data)
 {
   printk(KERN_DEBUG "sem: thread B entery\n");
 
+  set_current_state(TASK_INTERRUPTIBLE);
   schedule_timeout(msecs_to_jiffies(1000));
   up(&sem);
 
   printk(KERN_DEBUG "sem: thread B exit\n");
+    
   return 0;
 }
 
@@ -27,7 +31,7 @@ static int th_a_entery(void* data)
 {
   printk(KERN_DEBUG "sem: thread A entery\n");
   
-  init_MUTEX_LOCKED(&sem);
+  sema_init(&sem, 0);
   th_b = kthread_create(th_b_entery, 0, "th_b");
   if(NULL == th_b)
     {
@@ -37,24 +41,20 @@ static int th_a_entery(void* data)
   if(0 == wake_up_process(th_b))
     {
       printk(KERN_DEBUG "sem: can't start thread B\n");
-      goto th_a_exit:
+      goto th_a_exit;
     }
-  start_external_task(&sem);
 
   down(&sem);
   printk(KERN_DEBUG "sem: lock\n");
+  set_current_state(TASK_INTERRUPTIBLE);
   schedule_timeout(msecs_to_jiffies(1000));
-  printk(KERN_DEBUG "sem; unlock\n");
+  printk(KERN_DEBUG "sem: unlock\n");
   up(&sem);
 
  th_a_exit:
-
-  if(kthread_stop(th_b))
-    {
-      printk(KERN_DEBUG "sem: thread B stop fail\n")
-    }
-
+  
   printk(KERN_DEBUG "sem: thread A exit\n");
+ 
   return 0;
 }
 
@@ -63,14 +63,14 @@ static int __init sem_init(void)
   printk(KERN_DEBUG "sem: init\n");
 
   th_a = kthread_create(th_a_entery, 0, "th_a");
-  if(NULL = th_a)
+  if(NULL == th_a)
     {
-      printk(KERN_WARING "sem: thread A creation fail\n");
+      printk(KERN_WARNING "sem: thread A creation fail\n");
       return -ENOMEM;
     }
   if(0 == wake_up_process(th_a))
     {
-      printk(KERN_WARING "sem: can't start thread A\n");
+      printk(KERN_WARNING "sem: can't start thread A\n");
       sem_clenup();
       return -EBUSY;
     } 
@@ -82,20 +82,5 @@ module_init(sem_init);
 static void __exit sem_clenup(void)
 {
   printk(KERN_DEBUG "sem: clenup\n");
-
-  if(NULL == th_a)
-    {
-      if(kthread_stop(th_a))
-	{
-	  printk(KERN_WARING "sem: thread A stop fail\n");	
-	}
-    }
-
-  if(NULL == th_b)
-    {
-      if(kthread_stop(th_b))
-	{
-	  printk(KERN_WARING "sem: thread B stop fail\n");
-	}
-    }
 }
+module_exit(sem_clenup);
