fc25087debf4192f60031fea899b87ed35f499b2
Alexandr Kolosov <rikorsev@mail.ru>
2016-07-22 18:56:32 +0300

    added lktm_rw_semaphores.c, but still in work

---
+++ lktm_rw_semaphores.c
@@ -0,0 +1,81 @@
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/kthread.h>
+#include <linux/rwsem.h.
+#include <linux/delay.h>
+
+#define NUM_OF_WRITERS 2
+#define NUM_OF_READERS 3
+
+struct rw_semaphore sem;
+struct task_struct* writer[NUM_OF_WRITERS];
+struct task_struct* reader[NUM_OF_READERS];
+
+static int th_readers_entery(void* data)
+{
+  int n = *(int*)data;
+  
+  printk(KERN_DEBUG "rw_sem: READER thread %d entery\n", n);
+  
+  do{
+    
+  }while(false == kthread_should_stop());
+
+  printk(KERN_DEBUG "rw_sem: READER thread %d exit\n", n);
+  return 0;
+}
+
+static int th_writers_entery(void* data)
+{
+  int n = *(int*)data;
+
+  printk(KERN_DEBUG "rw_sem: WRITER thread %d entery\n", n);
+  
+  do{
+    
+  }while(false = kthread_should_stop());
+
+  printk(KERN_DEBUG "re_sem: WRITER thread &d exit\n", n);
+  return 0;
+}
+
+static int __init rw_sem_init(void)
+{
+  int i;
+  
+  printk(KERN_DEBUG "rw_sem: init\n");
+
+  init_rwsem(&sem);
+  
+  for( i = 0 ; i < NUM_OF_READERS ; i++ )
+    {
+      reader[i] = kthread_run( th_readers_entery, &i, "reader %d", i);
+    }
+
+  for( i = 0 ; i < NUM_OF_WRITERS ; i++)
+    {
+      writer[i] = kthread_run( th_writers_entery, &i, "writer %d", i);
+    }
+  
+  return 0;
+}
+module_init(rw_sem_init);
+
+static void __exit rw_sem_exit(void)
+{
+  int i;
+  
+  printk(KERN_DEBUG "rw_sem: clenup\n");
+  for( i = 0 ; i < NUM_OF_READERS ; i++)
+    {
+      if(NULL != reader[i]) kthread_stop(reader[i]);
+    }
+
+  for( i = 0 ; i < NUM_OF_WRITERS ; i++)
+    {
+      if(NULL != writer[i]) kthread_stop(writer[i]);
+    }
+  
+  printk(KERN_DEBUG "rw_sem: exit\n");
+}
+module_exit(rw_sem_exit);
