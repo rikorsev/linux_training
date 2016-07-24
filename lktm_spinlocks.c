8cf6c87cfbdeca70814125dc40e9e3d69e19fce0
Alexandr Kolosov <rikorsev@mail.ru>
2016-07-24 22:59:02 +0300

    lktm_spinlock.c done

--- lktm_spinlocks.c
+++ lktm_spinlocks.c
@@ -3,25 +3,44 @@
 #include <linux/spinlock.h>
 #include <linux/kthread.h>
 #include <linux/delay.h>
+#include <linux/timer.h>
+//#include <linux/getcpu.h>
+#include <linux/smp.h>
 
 DEFINE_SPINLOCK(lock);
-struct task_struct* th_a = NULL;
-struct task_struct* th_b = NULL;
+static struct task_struct* th_a = NULL;
+static struct task_struct* th_b = NULL;
+static struct timer_list tim;
 
+static void tim_handler(unsigned long data)
+{
+  printk(KERN_DEBUG "slock: timer handler entery\n");
+}
 
 static int th_a_entery(void* data)
 {
+  unsigned long flag;
+  
   printk(KERN_DEBUG "slock: thread A entery\n");
 
   do
     {
-      spin_lock(&lock);
+      printk(KERN_DEBUG "slock: thread A: CPU %d\n", smp_processor_id());
+      
+      tim.expires = jiffies + msecs_to_jiffies(100);
+      tim.function = tim_handler;
+      tim.data = 0;
+      add_timer(&tim);
+      printk(KERN_DEBUG "slock: tim go!\n");
+      
+      spin_lock_irqsave(&lock, flag);
       printk(KERN_DEBUG "slock: locked by thread A\n");
+      mdelay(200);
+      spin_unlock_irqrestore(&lock, flag);
+      printk(KERN_DEBUG "slock: unlocked by thread A\n");
       msleep(1000);
-      spin_unlock(&lock);
-      printk(KERN_DEBUG "slock: unlocked by thread B\n");
-      msleep(1000);
-    }while(false == kthread_should_stop);
+      
+    }while(false == kthread_should_stop());
 
   printk(KERN_DEBUG "slock: thread A exit\n");
   return 0;
@@ -33,10 +52,12 @@ static int th_b_entery(void* data)
   
   do
     {
+      printk(KERN_DEBUG "slock: thread B: CPU %d\n", smp_processor_id());
+      
       if(true == spin_trylock(&lock))
 	{
 	  printk(KERN_DEBUG "slock: locked by thread B\n");
-	  msleep(1000);
+	  mdelay(200);
 	  spin_unlock(&lock);
 	  printk(KERN_DEBUG "slock: unlocked by thread B\n");
 	}
@@ -44,7 +65,8 @@ static int th_b_entery(void* data)
 	{
 	  printk(KERN_DEBUG "slock: attempt to lock by thread B - fail \n");
 	}
-      msleep(1000);
+      msleep(300);
+      
     }while(false == kthread_should_stop());
 
   printk(KERN_DEBUG "slock: thread B exit \n");
@@ -64,6 +86,9 @@ module_init(slock_init);
 
 static void slock_exit(void)
 {
+  kthread_stop(th_a);
+  kthread_stop(th_b);
+  
   printk(KERN_DEBUG "slock: exit\n");
 }
 module_exit(slock_exit);
