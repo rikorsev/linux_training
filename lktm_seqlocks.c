80ff3400d4979f1598ca48d82c36595b016619e3
Alexandr Kolosov <rikorsev@mail.ru>
2016-08-02 19:56:26 +0300

    lktm_seqlocks.c has been debugged

--- lktm_seqlocks.c
+++ lktm_seqlocks.c
@@ -1,7 +1,6 @@
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/kthread.h>
-//#include <linux/rwsem.h>
 #include <linux/seqlock.h>
 #include <linux/delay.h>
 #include <linux/completion.h>
@@ -10,14 +9,14 @@
 #define NUM_OF_READERS 3
 
 static struct completion init_comp;
-//static struct rw_semaphore sem;
-static struct seqlock_t lock;
+static seqlock_t lock;
 static struct task_struct* writer[NUM_OF_WRITERS];
 static struct task_struct* reader[NUM_OF_READERS];
 
 static int th_readers_entery(void* data)
 {
   int n = (*(int*)data) + 1;
+  int count = 0;
 
   printk(KERN_DEBUG "seqlock: READER %d: entery\n", n);
 
@@ -28,17 +27,23 @@ static int th_readers_entery(void* data)
     do{
       
       count = read_seqbegin(&lock);
-      printk(KERN_DEBUG "seqlock: READER %d: attempt to read\n");
+      printk(KERN_DEBUG "seqlock: READER %d: attempt to read - START\n", n);
       mdelay(200);
-      if(read_seqretry(&lock, count)) break;
+      if(0 == read_seqretry(&lock, count)) break;
       printk(KERN_DEBUG "seqlock: READER %d: attempt to read - FAIL\n", n);
+      
+      if(true == kthread_should_stop()) goto reader_exit;
+
+      msleep(1000);
 
     }while(1);
 
-    printk(KERN_DEBUG " seqlock: READER %d: attempt to read - SUCCESS\n", n);
+    printk(KERN_DEBUG "seqlock: READER %d: attempt to read - SUCCESS\n", n);
     msleep(1000);
 
   }while(false == kthread_should_stop());
+
+ reader_exit:
 
   printk(KERN_DEBUG "seqlock: READER %d: exit\n", n);
   return 0;
@@ -58,8 +63,8 @@ static int th_writers_entery(void* data)
     printk(KERN_DEBUG "seqlock: WRITER %d: lock\n", n);
     mdelay(300);
     write_sequnlock(&lock);
-    printk(KERN_DEBUG "seqlock: WRITER &d: unlock\n", n);
-    msleep(1000);
+    printk(KERN_DEBUG "seqlock: WRITER %d: unlock\n", n);
+    msleep(300);
 
   }while(false == kthread_should_stop());
 
@@ -75,7 +80,6 @@ static int __init rw_sem_init(void)
 
   init_completion(&init_comp);
   seqlock_init(&lock);
-  //init_rwsem(&sem);
   
   for( i = 0 ; i < NUM_OF_READERS ; i++ )
     {
@@ -98,14 +102,15 @@ static void __exit rw_sem_exit(void)
   int i;
   
   printk(KERN_DEBUG "seqlock: clenup\n");
-  for( i = 0 ; i < NUM_OF_READERS ; i++)
-    {
-      if(NULL != reader[i]) kthread_stop(reader[i]);
-    }
 
   for( i = 0 ; i < NUM_OF_WRITERS ; i++)
     {
       if(NULL != writer[i]) kthread_stop(writer[i]);
+    }
+
+  for( i = 0 ; i < NUM_OF_READERS ; i++)
+    {
+      if(NULL != reader[i]) kthread_stop(reader[i]);
     }
   
   printk(KERN_DEBUG "seqlock: exit\n");
