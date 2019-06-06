a9f7f779ef6baf022ac6682cb2781d0a1f5b3c2a
Oleksandr Kolosov <oleksandr.kolosov@globallogic.com>
2019-06-06 18:50:48 +0300

    implement lktm_root object. Work in progress

--- lktm_kobj.c
+++ lktm_kobj.c
@@ -10,35 +10,57 @@ MODULE_AUTHOR("Oleksandr Kolosov");
 struct lktm_root
 {
   struct kobject        obj;   // kernel object structure
-  struct kobj_attribute attr;  // attribute structure
   u32                   state; // state variable
 };
 
-static ssize_t lktm_control_set(struct kobject *kobj, struct kobj_attribute *attr,  char *buf)
+/* this function shows current state of lktm_root object */
+static ssize_t lktm_state_show(struct kobject *kobj, struct attribute *attr, char *buf)
 {
+  struct lktm_root *root = container_of(kobj, struct lktm_root, obj);
 
+  printk(KERN_DEBUG "lktm: obj: show state\n");
+
+  /* what about copy to user ??? */
+
+  return sprintf(buf, "%s\n", root->state ? "active" : "disable");
 }
 
-static ssize_t lktm_control_get(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count)
+/* this function stores new state of lktm_root object */
+static ssize_t lktm_state_store(struct kobject *kobj, struct attribute *attr, const char *buf, size_t count)
 {
+  struct lktm_root *root = container_of(kobj, struct lktm_root, obj);
 
+  printk(KERN_DEBUG "lktm: obj: store state\n");
+
+  /* what about copy from user ??? */
+
+  sscanf(buf, "%du", &root->state);
+  return count;
 }
 
-/* devine control attribute */
-static struct kobj_attribute control = __ATTR(control, 0660, lktm_control_get,
-                                                             lktm_control_set);
+/* define state attribute */
+static struct attribute lktm_root_def_attr = {
+  .name = "state",
+  .mode = S_IRUSR | S_IWUSR
+};
 
+/* it is the array for default attributes */
+static struct attribute *lktm_root_def_attrs[] = {
+  &lktm_root_def_attr,
+  NULL
+};
 
 /* define sysfs operations for ktype */
-// const struct sysfs_ops lktm_root_sysfs_ops = 
-// {
-
-// };
+static struct sysfs_ops lktm_root_def_ops = {
+  .show = lktm_state_show,
+  .store = lktm_state_store
+};
 
 /* define ktype for lktm_obj */
-// static struct kobj_type lktm_root_ktype = {
-//   .sysfs_ops = &cache_sysfs_ops,
-// };
+static struct kobj_type lktm_root_ktype = {
+  .sysfs_ops = &lktm_root_def_ops,
+  .default_attrs = lktm_root_def_attrs
+};
 
 /* define lktm root object */
 struct lktm_root my_lktm_root = {0};
@@ -51,26 +73,11 @@ static int __init lktm_obj_init(void)
   printk(KERN_DEBUG "lktm: obj: init\n");
 
   /* let's init object structure and add it to sysfs*/
-  result = kobject_init_and_add(&my_lktm_root.obj, struct kobj_type *ktype, kernel_kobj, "lktm")
+  result = kobject_init_and_add(&my_lktm_root.obj, &lktm_root_ktype, kernel_kobj, "lktm");
   if(result < 0)
   {
     printk(KERN_WARNING "lktm: obj: kobject creation - fail\n");
     return result;
-  }
-
-
-
-
-  lktm_obj = kobject_create_and_add("lktm", kernel_kobj);
-  if(NULL == lktm_obj)
-  {
-    printk(KERN_WARNING "lktm: obj: kobject creation - fail\n");
-    return -1;
-  }
-
-  if(0 != sysfs_create_file(lktm_obj, &control.attr))
-  {
-    printk(KERN_WARNING "sysfs: file creation - fail\n");
   }
 
   return 0;
@@ -81,11 +88,8 @@ static void __exit lktm_obj_exit(void)
 {
   printk(KERN_DEBUG "lktm: obj: clenup\n");
 
-  /* remove control attribute */
-  sysfs_remove_file(lktm_obj, &control.attr);
-
   /* remove root object */
-  kobject_put(lktm_obj);
+  kobject_put(&my_lktm_root.obj);
 
   printk(KERN_DEBUG "lktm: obj: exit\n");
 }
