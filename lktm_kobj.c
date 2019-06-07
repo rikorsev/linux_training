a6876bbd3f138452024eeba32a7e92c68cdd0db5
Oleksandr Kolosov <oleksandr.kolosov@globallogic.com>
2019-06-07 18:44:10 +0300

    Still workink on lktm_kobj module. try to add children

--- lktm_kobj.c
+++ lktm_kobj.c
@@ -9,8 +9,8 @@ MODULE_AUTHOR("Oleksandr Kolosov");
 /* define structure for our root object */
 struct lktm_root
 {
-  struct kobject        obj;   // kernel object structure
-  u32                   state; // state variable
+  struct kobject        obj;           // kernel object structure
+  u32                   children_num;  // state variable
 };
 
 /* this function shows current state of lktm_root object */
@@ -18,23 +18,49 @@ static ssize_t lktm_state_show(struct kobject *kobj, struct attribute *attr, cha
 {
   struct lktm_root *root = container_of(kobj, struct lktm_root, obj);
 
-  printk(KERN_DEBUG "lktm: obj: show state\n");
+  printk(KERN_DEBUG "lktm: obj: show children number\n");
 
   /* what about copy to user ??? */
 
-  return sprintf(buf, "%s\n", root->state ? "active" : "disable");
+  return sprintf(buf, "%u\n", root->children_num);
 }
 
 /* this function stores new state of lktm_root object */
 static ssize_t lktm_state_store(struct kobject *kobj, struct attribute *attr, const char *buf, size_t count)
 {
+  //u8 i;
+  //struct kobject *child = NULL;
   struct lktm_root *root = container_of(kobj, struct lktm_root, obj);
 
-  printk(KERN_DEBUG "lktm: obj: store state\n");
+  printk(KERN_DEBUG "lktm: obj: store children number\n");
 
   /* what about copy from user ??? */
 
-  sscanf(buf, "%du", &root->state);
+  sscanf(buf, "%u", &root->children_num);
+
+  /* kill old children */
+  // list_for_each(current, to_kset(kobj)->list)
+  // {
+  //   child = container_of(current, struct kset, list)->;
+  // }
+
+  // /* create new cildren */
+  // for(i = 0; i < root->children_num; i++)
+  // {
+  //   child = kobject_create_and_add("child", kobj)
+  //   if(child == NULL)
+  //   {
+  //     printk(KERN_WARNING "lktm: obj: child birth - fail\n");
+  //     break;
+  //   }
+
+  //   if(kobject_set_name(child, "child%d", i) < 0)
+  //   {
+  //     printk(KERN_WARNING "lktm: obj: child rename - fail\n");
+  //     break;
+  //   }
+  // }
+
   return count;
 }
 
@@ -72,6 +98,8 @@ static int __init lktm_obj_init(void)
 
   printk(KERN_DEBUG "lktm: obj: init\n");
 
+  printk(KERN_DEBUG "kset: 0x%0p\n", my_lktm_root.obj.kset);
+
   /* let's init object structure and add it to sysfs*/
   result = kobject_init_and_add(&my_lktm_root.obj, &lktm_root_ktype, kernel_kobj, "lktm");
   if(result < 0)
@@ -79,6 +107,8 @@ static int __init lktm_obj_init(void)
     printk(KERN_WARNING "lktm: obj: kobject creation - fail\n");
     return result;
   }
+
+  printk(KERN_DEBUG "kset: 0x%0p\n", my_lktm_root.obj.kset);
 
   return 0;
 }
