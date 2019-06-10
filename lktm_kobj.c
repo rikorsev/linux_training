85b953ce75e1ca45c8108431ee9c0f47b942edb9
Oleksandr Kolosov <oleksandr.kolosov@globallogic.com>
2019-06-10 18:51:35 +0300

    finish with lktm_kobj kernel module. Some minor improvements are ahead :)

--- lktm_kobj.c
+++ lktm_kobj.c
@@ -2,115 +2,173 @@
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/uaccess.h> /* to access to user space*/
+#include <linux/slab.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Oleksandr Kolosov");
 
 /* define structure for our root object */
-struct lktm_root
+struct lktm_kobj
 {
-  struct kobject        obj;           // kernel object structure
-  u32                   children_num;  // state variable
+  struct kobject        kobj;  // kernel object structure
+  u32                   data;  // state variable
 };
 
 /* this function shows current state of lktm_root object */
-static ssize_t lktm_state_show(struct kobject *kobj, struct attribute *attr, char *buf)
+static ssize_t lktm_value_show(struct kobject *kobj, struct attribute *attr, char *buf)
 {
-  struct lktm_root *root = container_of(kobj, struct lktm_root, obj);
+  struct lktm_kobj *child = container_of(kobj, struct lktm_kobj, kobj);
 
-  printk(KERN_DEBUG "lktm: obj: show children number\n");
+  printk(KERN_DEBUG "lktm: obj: show value\n");
 
-  /* what about copy to user ??? */
-
-  return sprintf(buf, "%u\n", root->children_num);
+  return sprintf(buf, "%u\n", child->data);
 }
 
 /* this function stores new state of lktm_root object */
-static ssize_t lktm_state_store(struct kobject *kobj, struct attribute *attr, const char *buf, size_t count)
+static ssize_t lktm_value_store(struct kobject *kobj, struct attribute *attr, const char *buf, size_t count)
 {
-  //u8 i;
-  //struct kobject *child = NULL;
-  struct lktm_root *root = container_of(kobj, struct lktm_root, obj);
+  struct lktm_kobj *child = container_of(kobj, struct lktm_kobj, kobj);
 
-  printk(KERN_DEBUG "lktm: obj: store children number\n");
+  printk(KERN_DEBUG "lktm: obj: store value\n");
 
-  /* what about copy from user ??? */
-
-  sscanf(buf, "%u", &root->children_num);
-
-  /* kill old children */
-  // list_for_each(current, to_kset(kobj)->list)
-  // {
-  //   child = container_of(current, struct kset, list)->;
-  // }
-
-  // /* create new cildren */
-  // for(i = 0; i < root->children_num; i++)
-  // {
-  //   child = kobject_create_and_add("child", kobj)
-  //   if(child == NULL)
-  //   {
-  //     printk(KERN_WARNING "lktm: obj: child birth - fail\n");
-  //     break;
-  //   }
-
-  //   if(kobject_set_name(child, "child%d", i) < 0)
-  //   {
-  //     printk(KERN_WARNING "lktm: obj: child rename - fail\n");
-  //     break;
-  //   }
-  // }
-
+  sscanf(buf, "%u", &child->data);
   return count;
 }
 
+/* release function */
+static void lktm_release(struct kobject *kobj)
+{
+  struct lktm_kobj *child = container_of(kobj, struct lktm_kobj, kobj);
+
+  printk(KERN_DEBUG "lktm: obj: release %s\n", kobject_name(kobj));
+
+  kfree(child);
+}
+
 /* define state attribute */
-static struct attribute lktm_root_def_attr = {
-  .name = "state",
+static struct attribute lktm_def_attr = {
+  .name = "value",
   .mode = S_IRUSR | S_IWUSR
 };
 
 /* it is the array for default attributes */
-static struct attribute *lktm_root_def_attrs[] = {
-  &lktm_root_def_attr,
+static struct attribute *lktm_def_attrs[] = {
+  &lktm_def_attr,
   NULL
 };
 
 /* define sysfs operations for ktype */
-static struct sysfs_ops lktm_root_def_ops = {
-  .show = lktm_state_show,
-  .store = lktm_state_store
+static struct sysfs_ops lktm_def_ops = {
+  .show = lktm_value_show,
+  .store = lktm_value_store
 };
 
 /* define ktype for lktm_obj */
-static struct kobj_type lktm_root_ktype = {
-  .sysfs_ops = &lktm_root_def_ops,
-  .default_attrs = lktm_root_def_attrs
+static struct kobj_type lktm_ktype = {
+  .release = lktm_release,
+  .sysfs_ops = &lktm_def_ops,
+  .default_attrs = lktm_def_attrs
 };
 
 /* define lktm root object */
-struct lktm_root my_lktm_root = {0};
+static struct kset *parent_kset = NULL;
+
+/* create new object */
+static void lktm_create(u8 index)
+{
+  struct lktm_kobj *child = NULL;
+  int result = -1;
+
+  child = kzalloc(sizeof(*child), GFP_KERNEL);
+  if(child == NULL)
+  {
+    printk(KERN_DEBUG "lktm: obj: fail to create an object\n");
+    return;
+  }
+
+  child->kobj.kset = parent_kset;
+
+  result = kobject_init_and_add(&child->kobj, &lktm_ktype, NULL, "child%d", index);
+  if(result < 0)
+  {
+    printk(KERN_WARNING "lktm: obj: kobject init and add - fail. Result %d\n", result);
+    kfree(child);
+  }
+}
+
+static void lktm_del_all(void)
+{
+  struct kobject *k = NULL;
+  struct kobject *tmp = NULL;
+
+  list_for_each_entry_safe(k, tmp, &parent_kset->list, entry) {
+    kobject_put(k);
+  }
+}
+
+static u8 lktm_get_number(void)
+{
+  u8 count = 0;
+  struct list_head *pos = NULL;
+
+  list_for_each(pos, &parent_kset->list) {
+    count++;
+  }
+  return count;
+}
+
+static ssize_t lktm_kobj_create_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
+{
+  printk(KERN_DEBUG "lktm: obj: show number of created children\n");
+
+  return sprintf(buf, "%u\n", lktm_get_number());
+}
+
+static ssize_t lktm_kobj_create_store(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count)
+{
+  u32 number = 0;
+  u8 i;
+
+  printk(KERN_DEBUG "lktm: obj: store new children\n");
+
+  sscanf(buf, "%u", &number);
+
+  lktm_del_all();
+
+  for(i = 0; i < number; i++)
+  {
+    lktm_create(i);
+  }
+
+  return count;
+}
+
+static struct kobj_attribute create = __ATTR(create, S_IRUSR | S_IWUSR, lktm_kobj_create_show, lktm_kobj_create_store);
 
 /* init module */
 static int __init lktm_obj_init(void)
 {
-  int result = 0;
-
   printk(KERN_DEBUG "lktm: obj: init\n");
 
-  printk(KERN_DEBUG "kset: 0x%0p\n", my_lktm_root.obj.kset);
-
-  /* let's init object structure and add it to sysfs*/
-  result = kobject_init_and_add(&my_lktm_root.obj, &lktm_root_ktype, kernel_kobj, "lktm");
-  if(result < 0)
+  parent_kset = kset_create_and_add("lktm", NULL, kernel_kobj);
+  if(parent_kset == NULL)
   {
-    printk(KERN_WARNING "lktm: obj: kobject creation - fail\n");
-    return result;
+    printk(KERN_WARNING "lktm: obj: kset creation - fail\n");
+    return -1;
   }
 
-  printk(KERN_DEBUG "kset: 0x%0p\n", my_lktm_root.obj.kset);
+  if(0 != sysfs_create_file(&parent_kset->kobj, &create.attr))
+  {
+    printk(KERN_WARNING "lktm: obj: file creation - fail\n");
+    goto clean_kset;
+  }
 
   return 0;
+
+clean_kset:
+  kset_unregister(parent_kset);
+
+  return result;
 }
 module_init(lktm_obj_init);
 
@@ -118,8 +176,11 @@ static void __exit lktm_obj_exit(void)
 {
   printk(KERN_DEBUG "lktm: obj: clenup\n");
 
-  /* remove root object */
-  kobject_put(&my_lktm_root.obj);
+  lktm_del_all();
+
+  sysfs_remove_file(&parent_kset->kobj, &create.attr);
+
+  kset_unregister(parent_kset);
 
   printk(KERN_DEBUG "lktm: obj: exit\n");
 }
